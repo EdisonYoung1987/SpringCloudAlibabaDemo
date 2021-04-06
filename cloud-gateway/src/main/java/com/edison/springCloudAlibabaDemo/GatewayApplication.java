a77f637c44a594ba2cc73880038b0355dc6cd5aa
edisonyoung1987 <edison_young@aliyun.com>
2021-04-06 17:24:08 +0800

    1. 增加sentinel流量控制

--- cloud-gateway/src/main/java/com/edison/springCloudAlibabaDemo/GatewayApplication.java
+++ cloud-gateway/src/main/java/com/edison/springCloudAlibabaDemo/GatewayApplication.java
@@ -1,6 +1,7 @@
 package com.edison.springCloudAlibabaDemo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.joda.time.MutableDateTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +12,8 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
         log.info("--------------网关服务启动---------------");
+        MutableDateTime dateTime=new MutableDateTime();
+        dateTime.getYear();
     }
 
 }
---
+++ cloud-sentinel/src/main/java/com/edison/springCloudAlibabaDemo/SentinelDemoApp.java
@@ -0,0 +1,47 @@
+package com.edison.springCloudAlibabaDemo;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@Slf4j
+public class SentinelDemoApp {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context= SpringApplication.run(SentinelDemoApp.class, args);
+        log.info("-------------Sentinel Demo已启动-------------");
+        // 配置规则.
+        initFlowRules();
+
+        while (true) {
+            // 1.5.0 版本开始可以直接利用 try-with-resources 特性
+            try (Entry entry = SphU.entry("HelloWorld")) {
+                // 被保护的逻辑
+               log.info("hello world");
+            } catch (BlockException ex) {
+                // 处理被流控的逻辑
+                log.error("blocked!");
+            }
+        }
+    }
+    private static void initFlowRules(){
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(20);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }
+}
